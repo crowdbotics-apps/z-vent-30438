d06342c9a6a8feb2f5a2c75c90c2b14d4ab59110
Crowdbotics <team@crowdbotics.com>
2021-09-10 14:30:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "z_vent_30438Identifier",
+  url: "https://z_vent_30438Identifier.botics.co",
+  api: "https://z_vent_30438Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
